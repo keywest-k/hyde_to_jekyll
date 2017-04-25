04d302774b8c5fffbef2f37368fe8f63981955f9
hideyoshi-zx <underfox.underdog@gmail.com>
2017-04-25 14:32:00 +0900

    Modify base-uri

--- lib/hyde_to_jekyll/api_agent.rb
+++ lib/hyde_to_jekyll/api_agent.rb
@@ -4,7 +4,7 @@ require 'httparty'
 module HydeToJekyll
   class ApiAgent
     include HTTParty
-    base_uri 'darknessdevildb.local:3000'
+    base_uri 'keywest-hyde.herokuapp.com'
 
     def initialize(site_host)
       @site_host = site_host
